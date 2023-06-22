b1123250fe33c3caf758fee98757044435f0b246
txaty <txaty@proton.me>
2023-06-22 12:28:38 +0000

    Change configurations and add docker script.

---
+++ script/reset_docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+docker ps -aq | xargs docker stop | xargs docker rm
+
+docker volume prune -f
+docker network prune -f
+docker container prune -f
+
+docker run -d --name zkpos-redis -p 6379:6379 redis
+
+docker run -d --name zkpos-mysql -p 3306:3306  -v /server/docker_data/mysql_data:/var/lib/mysql -e MYSQL_USER=zkpos -e MYSQL_PASSWORD=zkpos@123 -e MYSQL_DATABASE=zkpos  -e MYSQL_ROOT_PASSWORD=zkpos@123 mysql
+
+docker run -d --name zkpos-kvrocks -p 6666:6666 -v /server/docker_data/kvrocksdata:/var/lib/kvrocks apache/kvrocks
